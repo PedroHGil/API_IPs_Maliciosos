b902048b49dfb50893c47c06e7b86f30edb1ad15
Pedro H Gil <80546377+PedroHGil@users.noreply.github.com>
2023-11-08 22:31:41 -0300

    umas coisas ai

--- controller/rotas.py
+++ controller/rotas.py
@@ -3,18 +3,26 @@ import uvicorn
 from service import abuse_ip
 from service import virus_total
 from model import request_body
-from service import alien_vault
+from service import url_scan
+from service import criminal_ip
 
 app = FastAPI()
 
 @app.post('/v1')
 def lambda_handler(ip_adress: request_body.IP):
-    alien_vault.get_alien(ip_adress)
     response = {"ABuseIP": abuse_ip.get_abuse(ip_adress), 
-                "VirusTotal": virus_total.get_virus(ip_adress)
+                "VirusTotal": virus_total.get_virus(ip_adress),
+                "CriminalIP": criminal_ip.get_criminal(ip_adress)
                }
     
     return response
+
+@app.post('/v2')
+def lambda_handler(ip_adress: request_body.IP):
+    response = {"URLScan": url_scan.get_urlscan(ip_adress)
+               }
+    return response
+
 
 @app.get('/v1/status')
 def status():
